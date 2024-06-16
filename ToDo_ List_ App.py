2265d0a9ee0bd8c6d6afd0fdee64cc3af9a91fea
wamisho_d <wamisho.bogale1989@gmail.com>
2024-06-16 15:43:33 -0400

    first commit

---
+++ ToDo_ List_ App.py
@@ -0,0 +1,78 @@
+def display_menu():
+    ''' display the main menu. '''
+    print('\nWelcome to the To-Do List App!')
+    print('Menu:')
+    print('1. Add a task')
+    print('2. view a tasks')
+    print('3. Mark a task as complete')
+    print('4. Delete a task')
+    print('5. Quit')
+def add_task(tasks):
+    ''' Add a new task to the to-do list. '''
+    title = input('Enter the task title:')
+    tasks.append({'title': title, 'status': 'Incomplete'})
+    print(f"Task '{title}' added.")
+def view_tasks(tasks):
+    ''' View all tasks in the to-do list '''
+    if not tasks:
+        print('No tasks availble')
+    else:
+        for idx, task in enumerate(tasks, start=1):
+            print(f"{idx}. {task['title']} - {task['status']}")
+def mark_task_complete(tasks):
+    ''' Mark a task as complete'''
+    try:
+        task_number = int(input("Enter a task number to mark as complete:"))
+        if 1 <= task_number <= len(tasks):
+            tasks[task_number -1]['status'] = 'complete'
+            print(f"Task '{tasks[task_number -1]['title']}' marked as complete.")
+        else:
+            print('invalid task number.')
+    except ValueError:
+        print('Enter a valid number')
+def delete_task(tasks):
+    ''' Delete a task from the to-do list'''
+    try:
+        task_number = int(input("Enter a task number to delete:"))
+        if 1 <= task_number <= len(tasks):
+            removed_task = tasks.pop(task_number -1)
+            print(f"Task '{removed_task['title']}' deleted.")
+        else:
+            print('invalid task number.')
+    except ValueError:
+        print('Enter a valid number')
+def main():
+    tasks = []
+    while True:
+        display_menu
+        choice = input('choose an option:')
+        if choice == '1':
+            add_task(tasks)
+        elif choice == '2':
+            view_tasks(tasks)
+        elif choice == '3':
+            mark_task_complete(tasks)
+        elif choice == '4':
+            delete_task(tasks)
+        elif choice == '5':
+            print('Quit the application')
+            break
+        else:
+            print('Invalid choice. Try again.')
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
